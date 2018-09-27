b1f569927ad37691630d44b58b4ae45ee7244256
MoonKuma <1244074267@qq.com>
2018-09-27 11:19:34 +0800

    App Annie Rank Search

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+@Software : PyCharm
+@Author : MoonKuma
+@Date  : 2018/9/27 11:17
+
+@Desc : 
+@File Structure : 
+@Related Libraries : 
---
+++ naive_method/app_annie_rank.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : app_annie_rank.py
+# @Author: MoonKuma
+# @Date  : 2018/9/27
+# @Desc  : Collect ranking data from App Annie
+#          This is an example for: 1. collecting AJAX (Asynchronous Javascript And XML) data
+#                                  2. solving commonly used anti-crawler mechanism
